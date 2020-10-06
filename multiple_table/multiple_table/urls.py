c5165aa09a46ac97d223c5b0310e665e944d10a8
esquire19 <72447832+esquire19@users.noreply.github.com>
2020-10-06 18:55:41 +0600

    Update urls.py
    i chnage on syntax

--- multiple_table/multiple_table/urls.py
+++ multiple_table/multiple_table/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('password_reset/confirm/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset'),
 
     path('',include('operation.urls')),
 
